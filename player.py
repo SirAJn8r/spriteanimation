ea8075c3c77c730b13d628e8097b69f716268b43
sirajn8r <hyperactive@live.com>
2018-04-27 20:04:17 -0700

    1st day

--- player.py
+++ player.py
@@ -6,12 +6,14 @@ class Player(pg.sprite.Sprite):
         super().__init__()
         bat = SpriteSheet("bat.gif", 6, 2)
         self.images = []
-        for i in range(6):
+        for i in range(5):
             self.images.append(bat.get_image(i, 0))
-        self.image = self.images[1]
+        self.image = pg.transform.scale(self.images[1], (100, 100))
         self.rect = self.image.get_rect()
         self.rect.center = pos
-
+        self.frame = 0
 
     def update(self):
         (width, height) = (pg.display.Info().current_w, pg.display.Info().current_h)
+        self.frame = pg.time.get_ticks() // 100 % 5
+        self.image = pg.transform.scale(self.images[self.frame], (100, 100))
